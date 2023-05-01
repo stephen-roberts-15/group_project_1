b73bad3586caaaa1acef809ad944bf70f1eee08b
Amanda Pham <amandampham@gmail.com>
2023-05-01 18:57:55 -0400

    added my code

--- api_key.py
+++ api_key.py
@@ -1,3 +1,5 @@
-api_key = "e2332978ac3146da918f8c4948219443"
+
 # Geoapify API Key
 geoapify_key = "e2332978ac3146da918f8c4948219443"
+# OpenWeatherMap API Key
+weather_api_key = "d5c7c5515e1490e64ea9671d04249086"
